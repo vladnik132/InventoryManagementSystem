2ffe650134b5a432994a86aad6cf2731a063ced7
Vladimir <nikolovv27@gmail.com>
2023-09-11 01:03:44 +0300

    Added a stored procedure

--- InventoryManagementSystem.sql
+++ InventoryManagementSystem.sql
@@ -1,13 +1,12 @@
---
--- Create InventoryManagementSystem Database
---
+-- Create InventoryManagementSystem database
 CREATE DATABASE InventoryManagementSystem
 GO
 
---
--- Create Suppliers Table
---
-CREATE TABLE Suppliers (
+USE InventoryManagementSystem
+
+-- Create Suppliers table
+CREATE TABLE Suppliers 
+(
     SupplierID INT PRIMARY KEY IDENTITY(1,1),
     SupplierName NVARCHAR(255),
     ContactName NVARCHAR(255),
@@ -15,10 +14,9 @@ CREATE TABLE Suppliers (
 );
 GO
 
---
--- Create InventoryItems Table
---
-CREATE TABLE InventoryItems (
+-- Create InventoryItems table
+CREATE TABLE InventoryItems 
+(
     ItemID INT PRIMARY KEY IDENTITY(1,1),
     ItemName NVARCHAR(255),
     Description NVARCHAR(1000),
@@ -29,10 +27,10 @@ CREATE TABLE InventoryItems (
 );
 GO
 
---
--- Create PurchaseOrders Table
---
-CREATE TABLE PurchaseOrders (
+
+-- Create PurchaseOrders table
+CREATE TABLE PurchaseOrders 
+(
     OrderID INT PRIMARY KEY IDENTITY(1,1),
     OrderDate DATE,
     SupplierID INT,
@@ -40,10 +38,9 @@ CREATE TABLE PurchaseOrders (
 );
 GO
 
---
--- Create PurchaseOrderDetails Table
---
-CREATE TABLE PurchaseOrderDetails (
+-- Create PurchaseOrderDetails table
+CREATE TABLE PurchaseOrderDetails 
+(
     OrderDetailID INT PRIMARY KEY IDENTITY(1,1),
     OrderID INT,
     ItemID INT,
@@ -53,9 +50,7 @@ CREATE TABLE PurchaseOrderDetails (
 );
 GO
 
---
 -- Insert sample data into Suppliers table
---
 INSERT INTO Suppliers 
 (
 	SupplierName, 
@@ -63,13 +58,15 @@ INSERT INTO Suppliers
 	ContactEmail
 )
 VALUES 
-('Supplier A', 'John Doe', 'john.doe@suppliera.com'),
-('Supplier B','Adam Baker', 'adam.baker@supplierb.com');
+	('Econt', 'John Brown', 'john.doe@econt.com'),
+	('Hermes', 'Martin Davis', 'martin.davis@hermes.com'),
+	('Econt', 'Adam Baker', 'adam.baker@econt.com'),
+	('Speedy', 'Stefani Doe', 'adam.baker@speedy.com'),
+	('Hermes', 'Pamela Monro', 'adam.baker@hermes.com'),
+	('Speedy', 'Melani Smith', 'adam.baker@speedy.com');
 GO
 
---
 -- Insert sample data into InventoryItems table
---
 INSERT INTO InventoryItems 
 (
 	ItemName, 
@@ -79,26 +76,30 @@ INSERT INTO InventoryItems
 	SupplierID
 )
 VALUES 
-('Laptop', 'High-performance laptop', 1000.00, 50, 1),
-('Smartphone','Low-performance smartphone',300.00,40,1);
+	('Laptop', 'High-performance laptop', 1099.99, 50, 1),
+	('Smartphone', 'Low-performance smartphone', 322.99, 37, 2),
+	('Camera', 'High-performance camera', 569.99, 45, 3),
+	('Laptop', 'Low-performance laptop', 454.99, 35, 1),
+	('Smartphone', 'High-performance smartphone', 749.99, 27, 2),
+	('Camera', 'Low-performance camera', 299.99, 58, 3);
 GO
 
---
 -- Insert sample data into PurchaseOrders table
---
 INSERT INTO PurchaseOrders 
 (
 	OrderDate, 
 	SupplierID
 )
 VALUES 
-	('2023-09-06', 1),
-	('2023-08-05', 1);
+	('2022-09-06', 3),
+	('2022-08-07', 1),
+	('2023-01-16', 2),
+	('2023-02-24', 3),
+	('2023-05-15', 1),
+	('2022-09-25', 2);
 GO
 
---
 -- Insert sample data into PurchaseOrderDetails table
---
 INSERT INTO PurchaseOrderDetails 
 (
 	OrderID, 
@@ -106,6 +107,42 @@ INSERT INTO PurchaseOrderDetails
 	Quantity
 )
 VALUES 
-(1, 1, 10),
-(2, 2, 9);
+	(1, 1, 20),
+	(1, 1, 90),
+	(1, 1, 40),
+	(1, 1, 50),
+	(1, 1, 60),
+	(1, 1, 10);
+GO
+
+-- This stored procedure shows a product above, equal or below chosen sales threshold 
+CREATE PROCEDURE GetSalesByThreshold
+	@ItemName NVARCHAR(255),
+	@Threshold INT,
+	@AboveEqualOrBelow NVARCHAR(10)
+AS
+BEGIN
+	IF @AboveEqualOrBelow = 'Above'
+	BEGIN
+		SELECT i.ItemName, pod.Quantity 
+		FROM InventoryItems AS i
+		INNER JOIN  PurchaseOrderDetails AS pod ON i.ItemID = pod.ItemID
+		WHERE i.ItemName = @ItemName
+			AND pod.Quantity > @Threshold;
+	END
+	ELSE IF @AboveEqualOrBelow = 'Below'
+	BEGIN
+		SELECT i.ItemName, pod.Quantity
+		FROM InventoryItems AS i
+		INNER JOIN PurchaseOrderDetails AS pod ON i.ItemID = pod.ItemID
+		WHERE i.ItemName = @ItemName
+			AND pod.Quantity < @Threshold;
+	END
+END;
+GO
+ 
+EXEC GetSalesByThreshold 
+	@ItemName = 'Laptop',
+	@Threshold = 20,
+	@AboveEqualOrBelow = 'Above';
 GO
